9d7d19b5c6a735360942e117d3873e0255d30798
heathdj <heathdj@gmail.com>
2022-02-06 16:42:26 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+/**
+ * File: /Users/heathdj/development/zombie-chat/backend/server.js
+ * Project: /Users/heathdj/development/zombie-chat/backend
+ * Created Date: Sunday, February 6th 2022, 4:41:56 pm
+ * Author: David Heath
+ * -----
+ * Last Modified:
+ * Modified By:
+ * -----
+ * Copyright (c) 2022 Zombie Chat
+ * ------------------------------------
+ * Javascript will save your soul!
+ */
